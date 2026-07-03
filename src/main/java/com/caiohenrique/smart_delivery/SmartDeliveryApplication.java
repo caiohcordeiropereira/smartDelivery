e673c7339c0195dc4fd1b2010efb669fefce8137
Caio Henrique Cordeiro Pereira <caio.cordeiropereira@gmail.com>
2026-07-03 18:34:09 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/caiohenrique/smart_delivery/SmartDeliveryApplication.java
@@ -0,0 +1,11 @@
+package com.caiohenrique.smart_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeliveryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/caiohenrique/smart_delivery/SmartDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiohenrique.smart_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
